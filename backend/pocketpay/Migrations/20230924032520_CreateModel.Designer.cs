6a21f4838e347f4116f03806622712337eb8f00f
João, o Garboso <j.pedrominuto@hotmail.com>
2023-09-24 00:43:08 -0300

    CreateWalletModel

--- backend/pocketpay/Migrations/20230924032520_CreateModel.Designer.cs
+++ backend/pocketpay/Migrations/20230924032520_CreateModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace pocketpay.Migrations
 {
     [DbContext(typeof(BankContext))]
-    partial class BankContextModelSnapshot : ModelSnapshot
+    [Migration("20230924032520_CreateModel")]
+    partial class CreateModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
---
+++ backend/pocketpay/Models/WalletModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pocketpay.Models
+{
+    [Table("Wallet")]
+    public class WalletModel
+    {
+        public Guid Id { get; set; }
+        public AccountModel Account { get; set; }   
+        public double Balance { get; set; }
+    }
+}
