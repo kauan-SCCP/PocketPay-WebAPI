3e187d37fec8f3f01a2ff09b1246467e12f705c9
Vinicius Rebelatto <vinicius-rebelatto@hotmail.com>
2023-09-30 13:37:05 -0300

    Repository criada

--- backend/pocketpay/Repositories/WalletRepository.cs
+++ backend/pocketpay/Repositories/WalletRepository.cs
@@ -1,4 +1,3 @@
-using System.Transactions;
 using Microsoft.EntityFrameworkCore;
 using pocketpay.Models;
 
@@ -14,14 +13,73 @@ public class WalletRepository : IWalletRepository
         _accountRepository = accountRepository;
     }
 
-    //Essa função serve para retornar todas as transações de um usuário através de seu email.
-
-    public async Task<WalletModel> getAccountWallet(string _email)
+    public async Task<WalletModel> Create(AccountModel account)
     {
-        var wallet = await  _context.Wallet 
+        var newWallet = new WalletModel();
+
+        newWallet.Id = new Guid();
+        newWallet.Account = account;
+        newWallet.Balance = 0;
+
+        await _context.AddAsync(newWallet);
+        await _context.SaveChangesAsync();
+
+        return newWallet;
+    }
+
+    public async Task<WalletModel?> Delete(Guid id)
+    {
+        var wallet = await FindById(id);
+        if (wallet == null) {return null;}
+
+        _context.Remove(wallet);
+        await _context.SaveChangesAsync();
+
+        return wallet;
+    }
+
+    public async Task<WalletModel?> Deposit(Guid id, double value)
+    {
+        var wallet = await FindById(id);
+        if (wallet == null) {return null;}
+
+        wallet.Balance += value;
+
+        _context.Update(wallet);
+        await _context.SaveChangesAsync();
+
+        return wallet;
+    }
+
+    public async Task<WalletModel?> FindByAccount(AccountModel account)
+    {
+        var wallet = await _context.Wallets
             .Include(wallet => wallet.Account)
-            .FirstOrDefaultAsync(wallet => wallet.Account.Email == _email);
-        
+            .FirstOrDefaultAsync(wallet => wallet.Account == account);
+
+        return wallet;
+    }
+
+    public async Task<WalletModel?> FindById(Guid id)
+    {
+        var wallet = await _context.Wallets
+            .Include(wallet => wallet.Account)
+            .FirstOrDefaultAsync(wallet => wallet.Id == id);
+
+        return wallet;
+    }
+
+
+    public async Task<WalletModel?> Withdraw(Guid id, double value)
+    {
+        var wallet = await FindById(id);
+        if (wallet == null) {return null;}
+
+        wallet.Balance -= value;
+
+        _context.Update(wallet);
+        await _context.SaveChangesAsync();
+
         return wallet;
     }
 }
