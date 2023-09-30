93189928c46bc45a15263a8e85c580ad4d29b7c4
Tandera, eye of <tandera@notemail.com>
2023-09-30 17:50:40 -0300

    Interface repository arrumado

--- backend/pocketpay/Repositories/Interfaces/ITransactionRepository.cs
+++ backend/pocketpay/Repositories/Interfaces/ITransactionRepository.cs
@@ -2,7 +2,7 @@ using pocketpay.Models;
 
 public interface ITransactionRepository
 {
-    public Task<TransactionModel> Create(AccountModel sender, AccountModel receiver);
+    public Task<TransactionModel> Create(AccountModel sender, AccountModel receiver, double value);
     public Task<TransactionModel?> FindById(Guid id);
     public Task<TransactionModel?> FindByAccount(AccountModel account);
     public Task<TransactionModel?> FindBySender(AccountModel sender);
