1f7f0a3ee5e1171c5d2bca37db999c1e6016b40d
Tandera, eye of <tandera@notemail.com>
2023-10-01 05:04:09 -0300

    Added model and repository

---
+++ backend/pocketpay/Models/CardModel.cs
@@ -0,0 +1,17 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Card")]
+public class CardModel
+{
+    [Key]
+    public Guid Id {get; set;}
+    public AccountModel? Account {get; set;}
+    public string? OwnerName {get; set;}
+    public CardType? Type {get; set;}
+    public string? Number {get; set;}
+    public string? CVV {get; set;}
+    public string? ExpirationDate {get; set;}
+    public bool IsActive {get; set;}
+}
---
+++ backend/pocketpay/Repositories/CardRepository.cs
@@ -0,0 +1,67 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class CardRepository : ICardRepository
+{
+    private readonly BankContext _context;
+
+    public CardRepository(BankContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CardModel> Create(AccountModel account, string ownerName, CardType type, string number, string cvv, string expirationDate)
+    {
+        var card = new CardModel()
+        {
+            Id = new Guid(),
+            Number = number,
+            Type = type,
+            CVV = cvv,
+            ExpirationDate = expirationDate,
+            Account = account,
+            IsActive = true,
+            OwnerName = ownerName
+        };
+
+        await _context.AddAsync(card);
+        await _context.SaveChangesAsync();
+        return card;
+    }
+
+    public async Task<CardModel?> Delete(Guid id)
+    {
+        var card = await FindById(id);
+
+        if (card == null) {return null;}
+        
+        _context.Remove(card);
+        await _context.SaveChangesAsync();
+
+        return card;
+    }
+
+    public async Task<IEnumerable<CardModel>> FindActive()
+    {
+        return await _context.Cards
+                .Include(card => card.Account)
+                .Where(card => card.IsActive).ToListAsync();
+    }
+
+    public async Task<IEnumerable<CardModel>> FindAll()
+    {
+        return await _context.Cards.ToListAsync();
+    }
+
+    public async Task<CardModel?> FindById(Guid id)
+    {
+        return await _context.Cards.Include(card => card.Account).FirstOrDefaultAsync(card => card.Id == id);
+    }
+
+    public async Task<IEnumerable<CardModel>> FindUnactive()
+    {
+        return await _context.Cards
+                        .Include(card => card.Account)
+                        .Where(card => !card.IsActive).ToListAsync();
+    }
+}
