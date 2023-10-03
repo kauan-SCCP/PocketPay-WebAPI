e07661d1cd7318a767a449cade0b07e2390110bf
Kauan Mendes <kauanalexandre2003@outlook.com>
2023-10-02 22:40:35 -0300

    transference repository finalizada

--- backend/pocketpay/Context/BankContext.cs
+++ backend/pocketpay/Context/BankContext.cs
@@ -11,7 +11,7 @@ public class BankContext : DbContext
 
     public DbSet<WalletModel> Wallets {get; set;}
     public DbSet<TransactionModel> Transactions {get; set;}
-
+    public DbSet<TransferenceModels> Transferences { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder options) 
     {
         options.UseSqlite($"Data Source={DbPath}");
---
+++ backend/pocketpay/Repositories/Interfaces/ITransferenceRepository.cs
@@ -0,0 +1,16 @@
+﻿using pocketpay.Models;
+
+namespace pocketpay.Repositories.Interfaces
+{
+    public interface ITransferenceRepository
+    {
+        public Task<TransferenceModels> Create(AccountModel sender, AccountModel receiver, double value);
+        public Task<TransferenceModels> FindById(Guid id); //Buscar um transação especifica
+        public Task<TransferenceModels> FindByTransaction(TransactionModel transaction); //
+        public Task<IEnumerable<TransferenceModels>> FindByAccount(AccountModel account);
+        public Task<IEnumerable<TransferenceModels>> FindBySender(AccountModel sender);
+        public Task<IEnumerable<TransferenceModels>> FindByReceiver(AccountModel receiver);
+        public Task<TransferenceModels> Revert(TransferenceModels transference);
+
+    }
+}
